3ce0d8ef1054d04314176ec93bc47a7d8c6ced0e
marination <maricadsouza221197@gmail.com>
2021-11-29 14:25:01 +0530

    fix: search_box_id instead of search_box_class & spaces to tabs

--- alfarsi_erpnext/public/js/nav_search.js
+++ alfarsi_erpnext/public/js/nav_search.js
@@ -3,26 +3,26 @@ erpnext.NavSearch = class {
 		this.make();
 	}
 
-    make() {
-        let navbar = $(".navbar-collapse").find(".ml-auto");
-        navbar.prepend(`
-            <li class="nav-item search-item mr-2">
-                <div class="input-group col-12 p-0 mr-4" style="width: 300px;">
-                    <div class="dropdown w-100" id="dropdownMenuSearch">
-                        <input type="search" name="query" id="nav-search-box" class="form-control font-md"
-                            placeholder="Search for Products"
-                            aria-label="Product" aria-describedby="button-addon2">
-                        <!-- Results dropdown rendered in product_search.js -->
-                    </div>
-                </div>
-            </li>
-        `);
+	make() {
+		let navbar = $(".navbar-collapse").find(".ml-auto");
+		navbar.prepend(`
+			<li class="nav-item search-item mr-2">
+				<div class="input-group col-12 p-0 mr-4" style="width: 300px;">
+					<div class="dropdown w-100" id="dropdownMenuSearch">
+						<input type="search" name="query" id="nav-search-box" class="form-control font-md"
+							placeholder="Search for Products"
+							aria-label="Product" aria-describedby="button-addon2">
+						<!-- Results dropdown rendered in product_search.js -->
+					</div>
+				</div>
+			</li>
+		`);
 
-        frappe.require('/assets/js/e-commerce.min.js', function() {
+		frappe.require('/assets/js/e-commerce.min.js', function() {
 			new erpnext.ProductSearch({
-                search_box_class: "#nav-search-box"
-            });
+				search_box_id: "#nav-search-box"
+			});
 		});
-    }
+	}
 }
 
